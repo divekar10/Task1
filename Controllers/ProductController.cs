0ab361292cd9546d90f3ca651f0cea4ea9cf4e2f
Akash <akashd251997@gmail.com>
2021-12-09 09:44:58 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult ProductList()
+        {
+            var product = new Product() { ProductName = "Mobile" };
+            return View();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String ProductName { get; set; }
+    }
+}
